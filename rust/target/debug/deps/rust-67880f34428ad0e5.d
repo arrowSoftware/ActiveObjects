a74aab3b044b94d2059b8ba8c041bf3f629edca3
Tyler Gajewski <tyler.gajewski01@gmail.com>
2023-06-23 16:40:10 -0400

    Removing target

--- rust/target/debug/deps/rust-67880f34428ad0e5.d
+++
@@ -1,6 +0,0 @@
-/home/tyler/Documents/Projects/ActiveObjects/rust/target/debug/deps/rust-67880f34428ad0e5: src/main.rs src/state_machine.rs
-
-/home/tyler/Documents/Projects/ActiveObjects/rust/target/debug/deps/rust-67880f34428ad0e5.d: src/main.rs src/state_machine.rs
-
-src/main.rs:
-src/state_machine.rs:
--- rust/target/debug/rust.d
+++
@@ -1,1 +0,0 @@
-/home/tyler/Documents/Projects/ActiveObjects/rust/target/debug/rust: /home/tyler/Documents/Projects/ActiveObjects/rust/src/main.rs /home/tyler/Documents/Projects/ActiveObjects/rust/src/state_machine.rs
