0aa185f65d937a3c312d1621849055a6c1ecc991
Tyler Gajewski <tyler.gajewski01@gmail.com>
2023-06-27 00:18:08 -0400

    Removing duplicate file

--- rust_attempt_2/src/Action.rs
+++
@@ -1,9 +0,0 @@
-use crate::state::StateT;
-
-/**
- * Return enum for each State.
- */
-pub enum Action {
-    Handled, /* Does nothing, just keeps the current state the same. */
-    TransitionTo(StateT) /* Sets the state machines current state to the new StateT State. */
-}
